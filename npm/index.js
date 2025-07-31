c0cadb40a20da24fafdf903eed0217d853ba189e
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-31 18:18:48 -0500

    Npm node.js

---
+++ npm/index.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log(chalk.green('Exito: NPM funciona correctamente.'));
+console.log(chalk.red('Esto es un error simulado'));
+console.log(chalk.blue('Información'));
+
+
