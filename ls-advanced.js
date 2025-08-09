6c6a7bbb1a7947d7858ec2c3eef35c69aed6cb9f
Miguel Angel <miguelangelgarb@gmail.com>
2025-08-09 18:21:32 -0500

    fs-node

---
+++ ls-advanced.js
@@ -0,0 +1,17 @@
+const fs = require('node:fs/promises');
+
+const folder = process.argv[2] ?? "../JavaScript";
+
+fs.readdir(folder)
+  .then((files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  })
+  .catch((err) => {
+    if (err) {
+      console.log("Error al leer el directorio: ", err);
+      return;
+    }
+  });
+
