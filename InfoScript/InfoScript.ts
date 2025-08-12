5ad41c46e55ac362382519d7ea7cf10950e70b3d
Miguel Angel <miguelangelgarb@gmail.com>
2025-08-12 17:50:15 -0500

    InfoScript

--- InfoScript/InfoScript.ts
+++ InfoScript/InfoScript.ts
@@ -3,10 +3,30 @@ import * as path from "node:path";
 import { readdir } from "node:fs";
 
 
-async function InfoSys () {
-    const plataforma: string = os.platform()
-    const memory: number = os.freemem()
-    const memoryFree: number = os.totalmem()
+function InfoSys() {
+
+    const platform = os.platform();
+    const memory = (os.totalmem() / (1024 * 1024)).toFixed(0);
+    const memoryFree = (os.freemem() / (1024 * 1024)).toFixed(0);
+    const userDir = os.homedir();
+
+    const Info = [platform, memory, memoryFree, userDir];
+    Info.forEach(Information => {
+        switch (Information) {
+            case platform:
+                console.log(`${"Plataforma:".padEnd(30)}${platform}`)
+                return;
+            case memory:
+                console.log(`${"Memoria:".padEnd(30)}${memory}mb`)
+                return;
+            case memoryFree:
+                console.log(`${"Memoria libre:".padEnd(30)}${memoryFree}mb`)
+                return;
+            case userDir:
+                console.log(`${"Directorio Personal:".padEnd(30)}${userDir}`)
+
+        }
+    });
 }
 
 InfoSys();
